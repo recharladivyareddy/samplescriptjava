c0fe7762ff7362c75a13ec69c3dc83afe5672b62
B MADHURIKA <68028934+budarajumadhurika@users.noreply.github.com>
2023-12-27 21:21:28 +0530

    Update App.java

--- src/main/java/in/kmit/ExampleJavaProject/App.java
+++ src/main/java/in/kmit/ExampleJavaProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Sample Hello World!" );
+        System.out.println( "I have changed this" );
     }
 }
